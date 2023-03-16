cc5b7858f74bf635d41dc8d511cf0c209d721f52
LaurenceFreeman <laurence.1.freeman@gmail.com>
2023-03-16 17:21:07 +0000

    Commiting point process model

---
+++ Point_Processes/homogeneous_point_process.py
@@ -0,0 +1,35 @@
+"""ALGORITHM 1: Homogeneous Poisson process; Taken from Thinning Algorithms for Simulating Point Processes 
+by Yuanda Chen, 2016, https://www.math.fsu.edu/~ychen/research/Thinning%20algorithm.pdf"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def homogeneous_poisson_process(lam, T):
+    n = 0
+    t0 = 0
+    times = []
+
+    while True:
+        u = np.random.uniform(0, 1)
+        w = -np.log(u) / lam # So that w ~ Exp(λ)
+        tn = t0 + w
+
+        if tn > T:
+            return times
+        else:
+            n += 1
+            t0 = tn
+            times.append(tn)
+
+# Example usage:
+lam = 3.0  # Rate λ
+T = 10.0   # Time interval [0, T]
+result = homogeneous_poisson_process(lam, T)
+
+# plot the result
+time = np.arange(0.0, T, 0.01)
+plt.eventplot(result)
+plt.xlabel("Time")
+plt.yticks([]) # Remove the y-axis ticks
+plt.title(f"Homogeneous Poisson process (λ = {lam}, T = {T})")
+plt.show()
