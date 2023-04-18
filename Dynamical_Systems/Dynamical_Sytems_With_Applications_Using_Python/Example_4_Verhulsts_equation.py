1799207d433607b51658ce364e09bb34eee55656
LaurenceFreeman <laurence.1.freeman@gmail.com>
2023-04-18 10:53:59 +0100

    bump

--- Dynamical_Systems/Dynamical_Sytems_With_Applications_Using_Python/Example_4_Verhulsts_equation.py
+++ Dynamical_Systems/Dynamical_Sytems_With_Applications_Using_Python/Example_4_Verhulsts_equation.py
@@ -19,6 +19,6 @@ print(sol)
 # result is a list of solutions to the ODE - 
 # Eq(p(t), B/(d*(1 - exp(B*(C1 - t)))))
 # This result can also be computed by hand 
-# test
+# This is very close to the logisitc equation
 
 # NOTE - When using dsolve the function such as P(t), x(t) must be written as such as not like P or x
